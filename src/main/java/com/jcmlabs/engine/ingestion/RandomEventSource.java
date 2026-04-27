b2ad59261a421c10e47550f98d646603d72013a6
JohnMalugu <johnmalugu99@gmail.com>
2026-04-27 11:21:56 +0300

    formatting on random event rule

--- src/main/java/com/jcmlabs/engine/ingestion/RandomEventSource.java
+++ src/main/java/com/jcmlabs/engine/ingestion/RandomEventSource.java
@@ -8,36 +8,28 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.function.Consumer;
 
-public class RandomEventSource implements EventSource{
+public class RandomEventSource implements EventSource {
     private final Random random = new Random();
-    private  volatile  boolean running = true;
+    private volatile boolean running = true;
 
 
     @Override
     public void start(Consumer<Event> eventConsumer) {
-        while(running){
+        while (running) {
             Event event = new Event(
-                    UUID.randomUUID(),
-                    "NETWORK_METRIC",
-                    Instant.now(),
-                    Map.of(
-                            "latency", random.nextInt(300),
-                            "packetLoss", random.nextDouble()
-                    )
-            );
+                    UUID.randomUUID(), "NETWORK_METRIC", Instant.now(), Map.of("latency", random.nextInt(300), "packetLoss", random.nextDouble()));
 
             eventConsumer.accept(event);
 
-            try{
+            try {
                 Thread.sleep(1);
-            }
-            catch (InterruptedException exception){
+            } catch (InterruptedException exception) {
                 Thread.currentThread().interrupt();
             }
         }
     }
 
-    public void stop(){
+    public void stop() {
         running = false;
     }
 }
