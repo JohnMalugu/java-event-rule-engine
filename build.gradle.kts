17117b2f13f8c031c89eb78a9e8880c4e25eaf03
JohnMalugu <johnmalugu99@gmail.com>
2026-04-28 13:23:49 +0300

    logging framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("ch.qos.logback:logback-classic:1.5.12")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
