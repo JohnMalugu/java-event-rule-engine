4942706a75211eaea8ece2ff4ad272b57c4d6a66
JohnMalugu <johnmalugu99@gmail.com>
2026-05-06 09:25:00 +0300

    mnt

--- src/main/java/com/jcmlabs/Main.java
+++ src/main/java/com/jcmlabs/Main.java
@@ -30,7 +30,6 @@ public class Main {
         //Start workers
         int workers = Runtime.getRuntime().availableProcessors();
 
-        //simpre looping
         for (int i = 0; i < workers; i++) {
             Thread worker = new Thread(new Worker(queue, eventProcessor));
             worker.start();
