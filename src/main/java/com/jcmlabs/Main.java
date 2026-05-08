e11b5f56c5f00438a57eebb7020e3110c7d77be6
JohnMalugu <johnmalugu99@gmail.com>
2026-05-08 08:31:10 +0300

    mnt

--- src/main/java/com/jcmlabs/Main.java
+++ src/main/java/com/jcmlabs/Main.java
@@ -35,7 +35,6 @@ public class Main {
             worker.start();
         }
 
-
         //Start event source
         EventSource source = new RandomEventSource();
         source.start(event -> {
